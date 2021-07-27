882f3b25cdd64cd31c1f2be91e24fdabe5040789
eternal <73202350+slow@users.noreply.github.com>
2021-07-27 04:06:26 +0100

    release

---
+++ README.md
@@ -0,0 +1,11 @@
+# Hide DM Buttons [![CodeFactor](https://www.codefactor.io/repository/github/slow/hide-dm-buttons/badge)](https://www.codefactor.io/repository/github/slow/hide-dm-buttons) [![GitHub issues](https://img.shields.io/github/issues/slow/hide-dm-buttons?style=flat)](https://github.com/slow/hide-dm-buttons/issues) [![GitHub stars](https://img.shields.io/github/stars/slow/hide-dm-buttons?style=flat)](https://github.com/slow/hide-dm-buttons/stargazers)
+
+A powercord plugin that hides every button in the DMs list except friends.
+
+# Requirements
+
+-  Powercord
+
+# Installation
+
+-  `git clone https://github.com/slow/hide-dm-buttons`
---
+++ index.js
@@ -0,0 +1,24 @@
+const { Plugin } = require('powercord/entities');
+const { getModule } = require('powercord/webpack');
+const { inject, uninject } = require('powercord/injector');
+
+const DMsList = getModule(m => m.default?.displayName == 'ConnectedPrivateChannelsList', false);
+
+module.exports = class extends Plugin {
+   startPlugin() {
+      inject('hide-dm-buttons', DMsList, 'default', (args, res) => {
+         let Arguments = args[0];
+         if (Arguments?.children) {
+            Arguments.children = [Arguments?.children?.find(i => i?.toString().includes('"friends"'))];
+         }
+
+         return args;
+      }, true);
+
+      DMsList.default.displayName = 'ConnectedPrivateChannelsList';
+   }
+
+   pluginWillUnload() {
+      uninject('hide-dm-buttons');
+   }
+};
